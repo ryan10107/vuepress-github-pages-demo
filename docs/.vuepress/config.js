0440e88bbbf50c26b7c29fc14090664b380ceab5
huangjian <huangjian@bigo.sg>
2022-04-13 10:40:27 +0800

    修改标题跳转url

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  title: 'kafka-book',
+  title: 'vuepress利用github actions部署github pages',
   description: '',
-  base: '/kafka-book/', // 这是部署到github相关的配置 下面会讲
+  base: '/vuepress-github-pages-demo/', // 这是部署到github相关的配置 下面会讲
   markdown: {
     lineNumbers: true // 代码块显示行号
   },
